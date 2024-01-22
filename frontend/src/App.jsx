c87b324e221df7b7ae618490ac6672c2131024a0
Kirill <faeeeee@gmail.com>
2024-01-22 18:31:23 +0500

    add logic of authorization checking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,25 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login/Login.jsx';
 import Home from './pages/Home/Home.jsx';
 import NotFound from './pages/NotFound/NotFound.jsx';
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext({ authStatus: false, setAuthStatus: () => {} });
 
 const App = () => {
+  const token = localStorage.getItem('token');
+  const [authStatus, setAuthStatus] = useState(!!token);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/404" />} />
-        <Route path="/404" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ setAuthStatus }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={authStatus ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/404" />} />
+          <Route path="/404" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,35 +1,65 @@
 import { Formik, Field, Form } from 'formik';
 import loginPageAvatar from '../../assets/login_page_avatar.jpg';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../App';
 
 const Login = () => {
-  useEffect(() => {
-    const testRequest = async () => {
-      const data = await axios.get('/api/v1/data');
-      console.log(data);
-    };
-    testRequest();
-  }, []);
+  const { setAuthStatus } = useContext(AuthContext);
+
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (values) => {
+    setIsInvalid(false);
+    try {
+      const { data: { token } } = await axios.post('/api/v1/login', { ...values });
+      localStorage.setItem('token', token);
+      setAuthStatus(true);
+      navigate('/');
+    } catch (error) {
+      setIsInvalid(true);
+      setAuthStatus(false);
+    }
+  };
+
+  const errorBlock = <div className="invalid-tooltip">Неверные имя пользователя или пароль</div>;
 
   const form = (
-    <Formik initialValues={{ email: '', password: '' }} onSubmit>
-      {() => (
-        <Form className="col-12 col-md-6 mt-3 mt-mb-0">
-          <h1 className="text-center mb-4">Войти</h1>
-          <div className="form-floating mb-3">
-            <Field type="name" name="username" className="form-control" autoComplete="username" required="" placeholder="Ваш ник" id="username" />
-            <label htmlFor="username">Ваш ник</label>
-          </div>
-          <div className="form-floating mb-4">
-            <Field type="password" name="password" className="form-control" autoComplete="current-password" required="" placeholder="Пароль" id="password" />
-            <label htmlFor="password">Password</label>
-          </div>
-          <button type="submit" className="w-100 mb-3 btn btn-outline-primary">
-            Войти
-          </button>
-        </Form>
-      )}
+    <Formik initialValues={{ username: '', password: '' }} onSubmit={(values) => handleLogin(values)}>
+      <Form className="col-12 col-md-6 mt-3 mt-mb-0">
+        <h1 className="text-center mb-4">Войти</h1>
+        <div className="form-floating mb-3">
+          <Field
+            type="name"
+            name="username"
+            className={`form-control ${isInvalid ? 'is-invalid' : ''}`}
+            autoComplete="username"
+            required
+            placeholder="Ваш ник"
+            id="username"
+          />
+          <label htmlFor="username">Ваш ник</label>
+        </div>
+        <div className="form-floating mb-4">
+          <Field
+            type="password"
+            name="password"
+            className={`form-control ${isInvalid ? 'is-invalid' : ''}`}
+            autoComplete="current-password"
+            required
+            placeholder="Пароль"
+            id="password"
+          />
+          <label htmlFor="password">Password</label>
+          {isInvalid && errorBlock}
+        </div>
+        <button type="submit" className="w-100 mb-3 btn btn-outline-primary">
+          Войти
+        </button>
+      </Form>
     </Formik>
   );
 
