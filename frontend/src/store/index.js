cafd895bd46083b24fee5f7fdd96a68f23e2a367
Kirill <faeeeee@gmail.com>
2024-01-25 12:46:16 +0500

    add app reducer to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { homeChannelsApi } from '../api/HomeChannelsApi';
 import { homeMessagessApi } from '../api/HomeMessagesApi';
+import appReducer from './slices/app.js';
 
 export default configureStore({
   reducer: {
     channels: homeChannelsApi.reducer,
     messages: homeMessagessApi.reducer,
+    app: appReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(homeChannelsApi.middleware, homeMessagessApi.middleware),
 });
