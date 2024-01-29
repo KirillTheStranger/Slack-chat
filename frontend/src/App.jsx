33765670a4a1a988f8742e1e4be29c1963e6e532
Kirill <faeeeee@gmail.com>
2024-01-29 16:56:25 +0500

    fix imports, add authSatus to context providet values

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import Home from './pages/Home.jsx';
+import SignUp from './pages/SingUp.jsx';
 import NotFound from './pages/NotFound.jsx';
-import { createContext, useState } from 'react';
 import NavBar from './components/NavBar.jsx';
+import { createContext, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export const AuthContext = createContext({ authStatus: false, setAuthStatus: () => {} });
 
@@ -12,18 +13,19 @@ const App = () => {
   const [authStatus, setAuthStatus] = useState(!!token);
 
   return (
-    <NavBar>
-      <AuthContext.Provider value={{ setAuthStatus }}>
+    <AuthContext.Provider value={{ authStatus, setAuthStatus }}>
+      <NavBar>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={authStatus ? <Home /> : <Navigate to="/login" />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={authStatus ? <Home /> : <Login />} />
+            <Route path="/signup" element={authStatus ? <Home /> : <SignUp />} />
             <Route path="*" element={<Navigate to="/404" />} />
             <Route path="/404" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </AuthContext.Provider>
-    </NavBar>
+      </NavBar>
+    </AuthContext.Provider>
   );
 };
 
