89e427961b90189f67bbdc73042f0aa543e4b85b
Kirill <faeeeee@gmail.com>
2024-01-29 16:57:22 +0500

    add exit button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,26 @@
+import { AuthContext } from '../App';
+import { useContext } from 'react';
+
 const NavBar = ({ children }) => {
+  const { authStatus, setAuthStatus } = useContext(AuthContext);
+
+  const handleExit = () => {
+    localStorage.clear();
+    setAuthStatus(false);
+  };
+
   return (
     <div className="d-flex flex-column h-100">
       <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
         <div className="container">
           <a className="navbar-brand" href="/">
-            Chat
+            Hexlet Chat
           </a>
+          {authStatus && (
+            <button className="btn btn-primary" onClick={handleExit}>
+              Выйти
+            </button>
+          )}
         </div>
       </nav>
       {children}
