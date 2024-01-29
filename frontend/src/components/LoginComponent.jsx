933511d168d69dab5cbfe7f34f0bbe570da23e63
Kirill <faeeeee@gmail.com>
2024-01-29 16:55:37 +0500

    change html structure

--- frontend/src/components/LoginComponent.jsx
+++ frontend/src/components/LoginComponent.jsx
@@ -1,6 +1,4 @@
-import avatar from '../assets/loginPage/avatar.jpg';
-
-const FormCoponent = ({ children }) => (
+const LoginComponent = ({ children, avatar }) => (
   <div className="container-fluid h-100">
     <div className="row justify-content-center align-content-center h-100">
       <div className="col-12 col-md-8 col-xxl-6">
@@ -11,10 +9,16 @@ const FormCoponent = ({ children }) => (
             </div>
             {children}
           </div>
+          <div className="card-footer p-4">
+            <div className="text-center">
+              <span>Нет аккаунта? </span>
+              <a href="/signup">Регистрация</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 );
 
-export default FormCoponent;
+export default LoginComponent;
