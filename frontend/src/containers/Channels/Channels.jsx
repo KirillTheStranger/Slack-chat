4a866744f2b1b1625b23a773f6ee9ebd2ead08c7
Kirill <faeeeee@gmail.com>
2024-01-30 17:19:23 +0500

    add lable for addchannel button

--- frontend/src/containers/Channels/Channels.jsx
+++ frontend/src/containers/Channels/Channels.jsx
@@ -33,6 +33,7 @@ const Channels = ({ channels }) => {
           onClick={() => dispatch(changeModalState({ isModalOpened: true, modalType: 'adding' }))}
         >
           <img src={addChannelImg} alt="Создать новый канал" width="20" height="20" />
+          <span className="visually-hidden">+</span>
         </button>
       </div>
       <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
