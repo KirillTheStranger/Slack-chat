7da7995db06e920e908423b753d01d2bab1757a8
Kirill <faeeeee@gmail.com>
2024-01-30 16:44:35 +0500

    add rollbar provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar.jsx';
 import { createContext, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import filter from 'leo-profanity';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 
 export const AuthContext = createContext({ authStatus: false, setAuthStatus: () => {} });
 
@@ -14,20 +15,31 @@ const App = () => {
   const [authStatus, setAuthStatus] = useState(!!token);
   filter.loadDictionary('ru');
 
+  const rollbarConfig = {
+    accessToken: 'e081cf636bbf48ac95ea490c16121610',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: 'production',
+  };
+
   return (
-    <AuthContext.Provider value={{ authStatus, setAuthStatus }}>
-      <NavBar>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={authStatus ? <Home /> : <Navigate to="/login" />} />
-            <Route path="/login" element={authStatus ? <Home /> : <Login />} />
-            <Route path="/signup" element={authStatus ? <Home /> : <SignUp />} />
-            <Route path="*" element={<Navigate to="/404" />} />
-            <Route path="/404" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </NavBar>
-    </AuthContext.Provider>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <AuthContext.Provider value={{ authStatus, setAuthStatus }}>
+          <NavBar>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={authStatus ? <Home /> : <Navigate to="/login" />} />
+                <Route path="/login" element={authStatus ? <Home /> : <Login />} />
+                <Route path="/signup" element={authStatus ? <Home /> : <SignUp />} />
+                <Route path="*" element={<Navigate to="/404" />} />
+                <Route path="/404" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </NavBar>
+        </AuthContext.Provider>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 
