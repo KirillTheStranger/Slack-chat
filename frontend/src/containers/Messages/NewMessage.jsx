9ba49c48b70b2567ed19b87b963d3633cf5cff63
Kirill <faeeeee@gmail.com>
2024-01-30 10:50:21 +0500

    fix autofocus issue when change channels

--- frontend/src/containers/Messages/NewMessage.jsx
+++ frontend/src/containers/Messages/NewMessage.jsx
@@ -1,13 +1,21 @@
 import sendButtonImg from '../../assets/homePage/sendButton.png';
 import { useAddMessageMutation } from '../../api/HomeMessagesApi.js';
+import { Formik } from 'formik';
 import { useSelector } from 'react-redux';
-import { Formik, Field } from 'formik';
+import { useRef, useEffect } from 'react';
+import { FormGroup, FormControl } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
 const NewMessage = () => {
   const [addMessage] = useAddMessageMutation();
   const { currentChannelId } = useSelector((state) => state.app);
   const { t } = useTranslation();
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const handleAddMessage = async (body, channelId, resetForm) => {
     const username = localStorage.getItem('username');
@@ -17,27 +25,27 @@ const NewMessage = () => {
   };
 
   return (
-    <div className="mt-auto px-5 py-3">
+    <FormGroup className="mt-auto px-5 py-3">
       <Formik initialValues={{ body: '' }} onSubmit={({ body }, { resetForm }) => handleAddMessage(body, currentChannelId, resetForm)}>
         {({ values, handleSubmit }) => (
           <form noValidate className="py-1 border rounded-2" onSubmit={handleSubmit}>
-            <div className="input-group has-validation">
-              <Field
+            <FormGroup className="input-group has-validation">
+              <FormControl
                 type="text"
                 name="body"
                 aria-label="Новое сообщение"
                 placeholder={t('homePage.newMessage')}
                 className="border-0 p-0 ps-2 form-control"
-                autoFocus
+                ref={inputRef}
               />
               <button type="submit" className="btn btn-group-vertical" disabled={!values.body.trim()} style={{ border: 'none' }}>
                 <img src={sendButtonImg} alt="Отправить сообщение" width="20" height="20" />
               </button>
-            </div>
+            </FormGroup>
           </form>
         )}
       </Formik>
-    </div>
+    </FormGroup>
   );
 };
 
