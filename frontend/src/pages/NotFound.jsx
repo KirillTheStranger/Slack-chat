45d80f4a446721505e761487b14371e26ce8f81f
Kirill <faeeeee@gmail.com>
2024-01-30 11:36:11 +0500

    edit 404page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
+import avatar from '../assets/notFoundPage/avatar.png';
+
 const NotFound = () => {
   return (
-    <div>
-      <p>404 NOT FOUND page</p>
+    <div className="text-center mt-5">
+      <img src={avatar} alt="Страница не найдена" className="img-fluid" />
+      <h1 className="h4 text-muted">Страница не найдена</h1>
+      <p className="text-muted">
+        Но вы можете перейти <a href="/">на главную страницу</a>
+      </p>
     </div>
   );
 };
