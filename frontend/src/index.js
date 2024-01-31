f606a430418b34ba1b7595ff80d37831538eea27
Kirill <faeeeee@gmail.com>
2024-01-31 14:21:11 +0500

    move init logic to own module

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
-import App from './App';
-import store from './store/index.js';
-import i18n from './i18next.js';
-import AuthProvider from './context/AuthProvider.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import init from './init.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
-root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <I18nextProvider i18n={i18n}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </I18nextProvider>
-    </React.StrictMode>
-  </Provider>,
-);
+
+root.render(init());
---
+++ frontend/src/init.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import App from './App.jsx';
+import store from './store/index.js';
+import i18n from './i18next.js';
+import AuthProvider from './context/AuthProvider.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const init = () => (
+  <Provider store={store}>
+    <React.StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </I18nextProvider>
+    </React.StrictMode>
+  </Provider>
+);
+
+export default init;
