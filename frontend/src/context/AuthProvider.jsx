9b4b548f46db19723e1f7347c500b13fdd0425ae
Kirill <faeeeee@gmail.com>
2024-02-01 12:09:58 +0500

    move state method into callback

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,11 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 import AuthContext from './AuthContext.js';
 
 const AuthProvider = ({ children }) => {
   const token = localStorage.getItem('token');
-  const [authStatus, setAuthStatus] = useState(!!token);
+  const [authStatus, setAuthenticationStatus] = useState(!!token);
+
+  const setAuthStatus = useCallback((status) => setAuthenticationStatus(status), []);
 
   const contextValue = useMemo(() => ({ authStatus, setAuthStatus }), [authStatus, setAuthStatus]);
 
