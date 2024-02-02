982eab0162723034634af6bab4a358c77abbdffa
Kirill <faeeeee@gmail.com>
2024-02-02 14:00:03 +0500

    move socket to init

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
+import { io } from 'socket.io-client';
 import App from './App.jsx';
 import store from './store/index.js';
 import i18n from './i18next.js';
 import AuthProvider from './context/auth/AuthProvider.jsx';
+import SocketProvider from './context/socket/SocketProvider.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const socket = io();
 
 const init = () => (
   <Provider store={store}>
     <React.StrictMode>
       <I18nextProvider i18n={i18n}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <SocketProvider socket={socket}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </SocketProvider>
       </I18nextProvider>
     </React.StrictMode>
   </Provider>
