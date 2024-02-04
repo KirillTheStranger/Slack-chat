db56592f677194a4ddbfb69836a3dad4a63b0056
Kirill <faeeeee@gmail.com>
2024-02-04 17:03:37 +0500

    create and add translation locales to notFoundPage

--- frontend/src/pages/notFound.jsx
+++ frontend/src/pages/notFound.jsx
@@ -1,15 +1,20 @@
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import avatar from '../assets/notFoundPage/avatar.png';
 
-const NotFound = () => (
-  <div className="text-center mt-5">
-    <img src={avatar} alt="Страница не найдена" className="img-fluid" />
-    <h1 className="h4 text-muted">Страница не найдена</h1>
-    <p className="text-muted">
-      Но вы можете перейти
-      {' '}
-      <a href="/">на главную страницу</a>
-    </p>
-  </div>
-);
+const NotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="text-center mt-5">
+      <img src={avatar} alt="Страница не найдена" className="img-fluid" />
+      <h1 className="h4 text-muted">{t('notFoundPage.header')}</h1>
+      <p className="text-muted">
+        {t('notFoundPage.body')}
+        {' '}
+        <Link to="/">{t('notFoundPage.link')}</Link>
+      </p>
+    </div>
+  );
+};
 
 export default NotFound;
