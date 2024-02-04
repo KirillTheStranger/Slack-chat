e57481339a231711d6cf90e7fb9bffb677ae53aa
Kirill <faeeeee@gmail.com>
2024-02-04 20:51:36 +0500

    move nonRemChan to components dir

---
+++ frontend/src/components/nonRemovableChannel.jsx
@@ -0,0 +1,5 @@
+import ChannelButton from '../containers/Channels/channelButton.jsx';
+
+const NonRemovableChannel = ({ channel }) => <ChannelButton channel={channel} />;
+
+export default NonRemovableChannel;
