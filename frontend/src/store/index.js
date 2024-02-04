2ea0a3bbf681aed6bae64dc033957a967fe43763
Kirill <faeeeee@gmail.com>
2024-02-04 19:27:03 +0500

    add authApi slice to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { homeChannelsApi } from '../api/homeChannelsApi';
 import { homeMessagessApi } from '../api/homeMessagesApi';
+import { authenticateApi } from '../api/authenticateApi.js';
 import appReducer from './slices/app.js';
 
 export default configureStore({
   reducer: {
     channels: homeChannelsApi.reducer,
     messages: homeMessagessApi.reducer,
+    authentication: authenticateApi.reducer,
     app: appReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-    .concat(homeChannelsApi.middleware, homeMessagessApi.middleware),
+    .concat(homeChannelsApi.middleware, homeMessagessApi.middleware, authenticateApi.middleware),
 });
