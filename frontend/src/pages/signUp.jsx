aa4e5dac5fdf58517cc372f96b1774fbca72e315
Kirill <faeeeee@gmail.com>
2024-02-04 20:33:07 +0500

    rename module, update logic

--- frontend/src/pages/signUp.jsx
+++ frontend/src/pages/signUp.jsx
@@ -1,6 +1,5 @@
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import {
   FormGroup, FormControl, Button, FormFloating, FormLabel,
@@ -9,11 +8,16 @@ import { useTranslation } from 'react-i18next';
 import SignupComponent from '../components/signupComponent.jsx';
 import avatar from '../assets/signupPage/avatar.png';
 import useAuthContext from '../hooks/useAuthContext.js';
+import { useSignUpMutation } from '../api/authenticateApi.js';
+import useLocalStorage from '../hooks/useLocalstorage.js';
 
 const SignUp = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const { setAuth } = useAuthContext();
+  const [signUp] = useSignUpMutation();
+
+  const setLocalStorageItem = useLocalStorage('set');
 
   const signupSchema = Yup.object().shape({
     username: Yup.string()
@@ -21,40 +25,40 @@ const SignUp = () => {
       .max(20, t('signupPage.errors.longUserName'))
       .required(t('signupPage.errors.requiredField')),
     password: Yup.string().min(6, t('signupPage.errors.shortPassword')).required(t('signupPage.errors.requiredField')),
-    confirmPassword: Yup.string().oneOf([Yup.ref('password')], t('signupPage.errors.passwordMatch')),
+    confirmPassword: Yup.string().oneOf([Yup.ref('password')], t('signupPage.errors.passwordMatch')).required(t('signupPage.errors.requiredField')),
   });
 
-  const handleSignup = async (values) => axios.post('/api/v1/signup', { ...values });
+  const handleFormSubmit = async (values, { setSubmitting, setErrors }) => {
+    try {
+      const { token, username } = await signUp({ ...values }).unwrap();
 
-  const handleFormSubmit = (values, { setSubmitting, setErrors }) => {
-    handleSignup(values)
-      .then(({ data }) => {
-        const { token, username } = data;
-        localStorage.setItem('token', token);
-        localStorage.setItem('username', username);
-        setSubmitting(false);
-        setAuth(true);
-        navigate('/');
-      })
-      .catch((error) => {
-        setSubmitting(false);
-        const { status } = error.response;
+      setLocalStorageItem('token', token);
+      setLocalStorageItem('username', username);
 
-        switch (status) {
-          case 0: {
-            setErrors({ username: ' ', password: ' ', confirmPassword: t('signupPage.errors.network') });
-            break;
-          }
-          case 409: {
-            setErrors({ username: ' ', password: ' ', confirmPassword: t('signupPage.errors.userExists') });
-            break;
-          }
-          default: {
-            setErrors({ username: ' ', password: ' ', confirmPassword: t('signupPage.errors.unknown') });
-            break;
-          }
+      setSubmitting(false);
+      setAuth(true);
+
+      navigate('/');
+    } catch (error) {
+      setSubmitting(false);
+
+      const { status } = error;
+
+      switch (status) {
+        case 0: {
+          setErrors({ username: ' ', password: ' ', confirmPassword: t('signupPage.errors.network') });
+          break;
         }
-      });
+        case 409: {
+          setErrors({ username: ' ', password: ' ', confirmPassword: t('signupPage.errors.userExists') });
+          break;
+        }
+        default: {
+          setErrors({ username: ' ', password: ' ', confirmPassword: t('signupPage.errors.unknown') });
+          break;
+        }
+      }
+    }
   };
 
   return (
@@ -71,7 +75,6 @@ const SignUp = () => {
         }) => (
           <Form className="w-50">
             <h1 className="text-center mb-4">{t('signupPage.form.header')}</h1>
-
             <FormFloating className="mb-3">
               <FormControl
                 name="username"
