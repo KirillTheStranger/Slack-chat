78e907f623b2bc66be55dbe565cd509730e46555
Kirill <faeeeee@gmail.com>
2024-02-04 19:26:34 +0500

    create authApi slice

---
+++ frontend/src/api/authenticateApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import useSetHeaders from '../hooks/useSetHeaders';
+
+export const authenticateApi = createApi({
+  reducerPath: 'authentication',
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api/v1/',
+    prepareHeaders: useSetHeaders,
+  }),
+  endpoints: (builder) => ({
+    login: builder.query({
+      query: () => ({
+        url: 'login',
+        method: 'GET',
+      }),
+    }),
+    signUp: builder.mutation({
+      query: (newUserData) => ({
+        url: 'signup',
+        method: 'POST',
+        body: newUserData,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginQuery, useSignUpMutation } = authenticateApi;
