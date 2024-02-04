e92fa61ef87af6d3cf191afaa446e72351cc21cd
Kirill <faeeeee@gmail.com>
2024-02-04 20:54:04 +0500

    swap Button component to ChannelButton

--- frontend/src/containers/Channels/removableChannel.jsx
+++ frontend/src/containers/Channels/removableChannel.jsx
@@ -1,9 +1,9 @@
 import { useSelector, useDispatch } from 'react-redux';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
-import Button from 'react-bootstrap/Button';
 import { useTranslation } from 'react-i18next';
-import { changeChannel, changeModalState } from '../../store/slices/app.js';
+import { changeModalState } from '../../store/slices/app.js';
+import ChannelButton from './channelButton.jsx';
 
 const RemovableChannel = ({ channel }) => {
   const dispatch = useDispatch();
@@ -13,14 +13,7 @@ const RemovableChannel = ({ channel }) => {
 
   return (
     <Dropdown as={ButtonGroup} className="d-flex">
-      <Button
-        variant={`${currentChannel === channel.name ? 'secondary' : ''}`}
-        className="w-100 rounded-0 text-start text-truncate"
-        onClick={() => dispatch(changeChannel({ name: channel.name, id: channel.id }))}
-      >
-        <span className="me-1">#</span>
-        {channel.name}
-      </Button>
+      <ChannelButton channel={channel} />
 
       <Dropdown.Toggle variant={`${currentChannel === channel.name ? 'secondary' : ''}`} className="flex-grow-0 dropdown-toggle-split">
         <span className="visually-hidden">{t('homePage.setupChannel')}</span>
